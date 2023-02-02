052b6e59e717533ce40a0b4299c05e744e9906e0
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2023-02-02 15:39:10 +0530

    palindrome changes

--- palindrome.pl
+++ palindrome.pl
@@ -8,8 +8,8 @@ reverse([H|T],R):- reverse(T,A),concat(A,[H],R),!.
 
 % EQUAL 
 equal([],[]):-write('List is palindrome').
-equal([H1|L1],[H2|L2]):-write('List is not a palindrome').
 equal([H1|L1],[H1|L2]):-equal(L1,L2).
+equal([H1|L1],[H2|L2]):-write('List is not a palindrome').
 
 % PALINDROME
 pal(L):-reverse(L,R),equal(L,R),!.
